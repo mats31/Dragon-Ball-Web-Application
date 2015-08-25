f1a166723b76686a881eab214bef995b6d37bd14
Administrator <admin@example.com>
2015-08-25 09:07:57 +0200

    Add text to application

--- js/ParticleSystem.js
+++ js/ParticleSystem.js
@@ -42,10 +42,11 @@ var ParticleSystem = function(){
 	this.pathImg = 'img/';
 	this.loading = false;
 
-	//tests
-	this.cube;
-	this.log = [];
-
+	// Panel
+	this.name = document.getElementById('name');
+	this.subtitle = document.getElementById('subtitle');
+	this.description = document.getElementById('description');
+	this.ki = document.getElementById('ki');
 };
 
 ParticleSystem.prototype.init = function(){
@@ -80,8 +81,8 @@ ParticleSystem.prototype.init = function(){
 	this.scene = new THREE.Scene();
 	this.scene.add(this.camera);
 
-	// Set z position for camera
-	this.camera.position.x = 335;
+	// Set positions for camera
+	this.camera.position.x = (this.width / 2) / 2.4;
 	this.camera.position.y = -300;
 	this.camera.position.z = 1000;
 
@@ -163,12 +164,6 @@ ParticleSystem.prototype.appendRenderer = function(){
     this.particleSystem.position.set(0,0,0);
     this.scene.add(this.particleSystem);
 
-   var geometry = new THREE.BoxGeometry( 100, 100, 100 );
-   var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-   this.cube = new THREE.Mesh( geometry, material );
-   this.scene.add( this.cube );
-   this.cube.position.set(500, 200, 0);
-
    this.update();
 };
 
@@ -188,6 +183,7 @@ ParticleSystem.prototype.createParticles = function(){
 	};
 
 	this.changeCharacter();
+	this.updatePanel();
 };
 
 ParticleSystem.prototype.updateParticles = function(){
@@ -199,45 +195,6 @@ ParticleSystem.prototype.updateParticles = function(){
 		i = 0,
 		j = 0,
 		that = this;
-
-	// this.nextParticles.vertices = [];
-	// this.nextParticles.colors   = []; 
-
-	// for (var j = 0; j < this.particles.vertices.length; j++) {
-		
-	// 	this.particles.colors[j] = new THREE.Color('black');
-	// };
-
-	// for (x = 0; x < 174 * 4; i++) {
-
-	// 	if (x<this.img.width *4) {
-
-	// 		if (y >= 0) {
-
-	// 			var p = ((y * this.img.width * 4) + x);
-
-	// 			if(pixels.data[p+3] > 0) {
-
-	// 				var pixelCol	= (pixels.data[p] << 16) + (pixels.data[p+1] << 8) + pixels.data[p+2];
-	// 				var color 		= new THREE.Color(pixelCol);
-	// 				var vector 		= new THREE.Vector3(-this.img.width/2 + x/4, -y, 0);
-					
-	// 				// push on the particle
-	// 				this.particles.vertices[i] = vector;
-	// 				this.particles.colors[i] = color;
-	// 			}
-
-	// 			y-= this.density;
-	// 		};
-
-	// 		x+= step;
-
-	// 	} else {
-
-	// 		this.particles.vertices[i] = new THREE.Vector3(0,0,0);
-	// 		this.particles.colors[i] = new THREE.Color('black');
-	// 	}
-	// };
 
 	for(x = 0; x < this.maxParticles; x+= step) {
 
@@ -265,10 +222,6 @@ ParticleSystem.prototype.updateParticles = function(){
     				createjs.Tween.get(this.particles.vertices[j])
     				    .to({z: Math.floor(Math.random()*101) - 50}, 1500, createjs.Ease.QuartIn)
     				    .to({z: 0}, 1500, createjs.Ease.QuartIn);
-    				// createjs.Tween.get(this.particles.vertices[j])
-    				//     .to({x:0, y:0, z:0}, 1500, createjs.Ease.QuartIn)
-    				//     .to({x:vector.x,y:vector.y,z: 0}, 1500, createjs.Ease.QuartIn);
-
 			    } else {
 			    	var color 		= new THREE.Color('black');
 			    	var vector 		= new THREE.Vector3(9999, 9999, 0);
@@ -290,36 +243,6 @@ ParticleSystem.prototype.updateParticles = function(){
 			j++;	
     	}
     }
-    //setTimeout(function(){
-    //this.particles.__webglParticleCount = this.nextParticles.vertices.length;
-    //this.particles.__colorArray = this.particles.__colorArray.slice(0, this.nextParticles.vertices.length*3);
-    //this.particles.__vertexArray = this.particles.__vertexArray.slice(0, this.nextParticles.vertices.length*3);
-    //},500);
-    // this.particles.vertices = this.particles.vertices.slice(0, this.nextParticles.vertices.length);
-    // this.particles.colors = this.particles.colors.slice(0, this.nextParticles.vertices.length);
-    // this.log.push(pixels);
-    // if (this.particles.vertices.length > this.nextParticles.vertices.length) {
-    // 	var surplus = this.particles.vertices.length - this.nextParticles.vertices.length,
-    // 		end = false;
-
-    // 	this.particles.vertices = this.particles.vertices.splice(0, this.nextParticles.vertices);
-    // 	this.particles.colors = this.particles.colors.splice(0, this.nextParticles.vertices);
-
-    // 	for (surplus; surplus < this.particles.vertices.length; surplus++) {
-	   //  	// createjs.Tween.get(this.particles.colors[surplus])
-			 //   //  .to({r: 0, g: 0, b: 0}, 2000, createjs.Ease.QuartIn);
-			 //   this.particles.vertices.splice(surplus, 1);
-    // 	};
-    // };
-
- //    this.limits = {
- //    	"limit":[],
- //    	"done":[]
- //    };
- //    for (var i = 0; i < this.particles.vertices.length; i++) {
-	// 	this.limits.limit[i] = Math.floor(Math.random()*101) - 50;
-	// 	this.limits.done[i] = false;
-	// };
 };
 
 ParticleSystem.prototype.loadImage = function(image){
@@ -389,6 +312,7 @@ ParticleSystem.prototype.previousCharacter = function(){
 	if (this.step - 1 >= 0){
 		this.step--;
 		this.changeCharacter();
+		this.updatePanel();
 	}
 };
 
@@ -400,6 +324,7 @@ ParticleSystem.prototype.nextCharacter = function(){
 	if (this.step + 1 <= this.datas.characters.length){
 		this.step++;
 		this.changeCharacter();
+		this.updatePanel();
 	}
 };
 
@@ -421,6 +346,39 @@ ParticleSystem.prototype.changeCharacter = function(){
 	};
 }
 
+ParticleSystem.prototype.updatePanel = function(){
+
+	this.name.innerHTML = '';
+	this.subtitle.innerHTML = '';
+	this.description.innerHTML = '';
+
+	this.showText(this.name, this.datas.characters[this.step].name, 0, 100);
+	this.showText(this.subtitle, this.datas.characters[this.step].subtitle, 0, 50);
+	this.showText(this.description, this.datas.characters[this.step].description, 0, 5);
+	this.showKi(this.ki, this.datas.characters[this.step].ki);
+};
+
+ParticleSystem.prototype.showText = function(target, message, index, interval){
+
+	var that = this;
+
+	if (index < message.length) { 
+		target.innerHTML += message[index++]; 
+		setTimeout(function () { that.showText(target, message, index, interval); }, interval);
+	} 
+};
+
+ParticleSystem.prototype.showKi = function(target, ki){
+	var options = {
+	  useEasing : true, 
+	  useGrouping : true, 
+	  separator : ' '
+	},
+	countUp = new CountUp(target,0,ki,0,3,options);
+
+	countUp.start();
+};
+
 ParticleSystem.prototype.update = function(){
 	requestAnimationFrame(this.update.bind(this));
 	this.render();
@@ -430,86 +388,9 @@ ParticleSystem.prototype.render = function render(){
 	if (this.updating) {
 		this.particleSystem.geometry.verticesNeedUpdate = true;
 		this.particleSystem.geometry.colorsNeedUpdate = true;
-
-		// var xStep = 1,
-		// 	yStep = 4,
-		// 	zStep = 0.5;
-
-		// for (var i = 0; i < this.nextParticles.vertices.length; i++) {
-
-		// 	var end = true;
-
-			// if (this.limits.limit[i] > 0 && this.particles.vertices[i].z < this.limits.limit[i] && this.limits.done[i] == false)
-			// 	this.particles.vertices[i].z += zStep;
-			// else if (this.limits.limit[i] < 0 && this.particles.vertices[i].z > this.limits.limit[i] && this.limits.done[i] == false)
-			// 	this.particles.vertices[i].z -= zStep;
-
-			// console.log(this.limits.done[i]);
-			
-			// if (this.limits.limit[i] > 0 && this.particles.vertices[i].z == this.limits.limit[i] && this.particles.vertices[i].z > 0){
-			// 	this.limits.done[i] = true;
-			// 	if (this.limits.done[i])
-			// 		this.particles.vertices[i].z -= zStep;
-			// } else if (this.limits.limit[i] < 0 && this.particles.vertices[i].z == this.limits.limit[i] && this.particles.vertices[i].z < 0){
-			// 	this.limits.done[i] = true;
-			// 	if (this.limits.done[i])
-			// 		this.particles.vertices[i].z += zStep;
-			// }
-
-
-			// if (typeof this.particles.vertices[i] != 'undefined') {
-
-			// 	if (this.nextParticles.vertices[i].x > this.particles.vertices[i].x) {
-			// 		if (this.particles.vertices[i].x >= this.nextParticles.vertices[i].x - xStep)
-			// 			this.particles.vertices[i].x = this.nextParticles.vertices[i].x;
-			// 		else
-			// 			this.particles.vertices[i].x += xStep;
-			// 	}
-
-			// 	if (this.nextParticles.vertices[i].x < this.particles.vertices[i].x) {
-			// 		if (this.particles.vertices[i].x <= this.nextParticles.vertices[i].x + xStep)
-			// 			this.particles.vertices[i].x = this.nextParticles.vertices[i].x;
-			// 		else
-			// 			this.particles.vertices[i].x -= xStep;
-			// 	}
-
-			// 	if (this.nextParticles.vertices[i].y > this.particles.vertices[i].y) {
-			// 		if (this.particles.vertices[i].y >= this.nextParticles.vertices[i].y - yStep)
-			// 			this.particles.vertices[i].y = this.nextParticles.vertices[i].y;
-			// 		else
-			// 			this.particles.vertices[i].y += yStep;
-			// 	}
-
-			// 	if (this.nextParticles.vertices[i].y < this.particles.vertices[i].y) {
-			// 		if (this.particles.vertices[i].y <= this.nextParticles.vertices[i].y + yStep)
-			// 			this.particles.vertices[i].y = this.nextParticles.vertices[i].y;
-			// 		else
-			// 			this.particles.vertices[i].y -= yStep;
-			// 	}
-			// };
-		//};
-
-		// var end = true;
-
-		// for (var i = 0; i < this.particles.vertices.length; i++) {
-			
-		// 	if (this.limits[i] > 0 && this.particles.vertices[i].z < this.limits[i]) {
-		// 		this.particles.vertices[i].z += 1;
-		// 		end = false;
-		// 	} else if (this.limits[i] < 0 && this.particles.vertices[i].z > this.limits[i]) {
-		// 		this.particles.vertices[i].z -= 1;
-		// 		end = false;
-		// 	}
-
-		// 	if(i == this.particles.vertices.length - 1 && end)
-		// 		this.changeCharacter();
-
-		// };		
 	}
 
   	this.particleSystem.rotation.y += 0.015;
-  	this.cube.rotation.y += 0.015;
-  	
 	this.renderer.render(this.scene, this.camera);
 };
 
